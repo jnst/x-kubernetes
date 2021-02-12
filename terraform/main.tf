6ae36a121381e7c88603f01ee36fbc547f2fc5b8
jnst <jnst@outlook.com>
2021-02-12 22:32:51 +0900

    Add vm instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,24 +10,24 @@ terraform {
 provider "google" {
   credentials = var.GOOGLE_CREDENTIALS
   project     = var.PROJECT_ID
-  region      = "us-west1"
-  zone        = "us-west1-c"
+  region      = var.mainly_region
+  zone        = var.mainly_zone
 }
 
 resource "google_compute_network" "network" {
-  name                    = "tf-network"
+  name                    = "tf-xk8s-network"
   auto_create_subnetworks = false
   routing_mode            = "REGIONAL"
 }
 
 resource "google_compute_subnetwork" "subnetwork" {
-  name          = "tf-subnetwork"
+  name          = "tf-xk8s-subnetwork"
   ip_cidr_range = "10.240.0.0/24"
   network       = google_compute_network.network.id
 }
 
 resource "google_compute_firewall" "firewall_internal" {
-  name      = "tf-firewall-internal"
+  name      = "tf-xk8s-firewall-internal"
   network   = google_compute_network.network.id
   direction = "INGRESS"
 
@@ -45,7 +45,7 @@ resource "google_compute_firewall" "firewall_internal" {
 }
 
 resource "google_compute_firewall" "firewall_external" {
-  name      = "tf-firewall-external"
+  name      = "tf-xk8s-firewall-external"
   network   = google_compute_network.network.id
   direction = "INGRESS"
 
@@ -58,4 +58,46 @@ resource "google_compute_firewall" "firewall_external" {
   }
 
   source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_instance" "master_node" {
+  for_each                  = local.master_nodes
+  name                      = "tf-xk8s-instance-${each.key}"
+  machine_type              = "f1-micro"
+  zone                      = var.mainly_zone
+  tags                      = ["xk8s", "master"]
+  allow_stopping_for_update = true
+  scheduling {
+    preemptible       = each.value
+    automatic_restart = false
+  }
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2004-lts"
+    }
+  }
+  network_interface {
+    network = google_compute_network.network.id
+  }
+}
+
+resource "google_compute_instance" "worker_node" {
+  for_each                  = local.worker_nodes
+  name                      = "tf-xk8s-instance-${each.key}"
+  machine_type              = "f1-micro"
+  zone                      = var.mainly_zone
+  tags                      = ["xk8s", "worker"]
+  allow_stopping_for_update = true
+  scheduling {
+    preemptible       = each.value
+    automatic_restart = false
+  }
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2004-lts"
+    }
+  }
+  network_interface {
+    network = google_compute_network.network.id
+  }
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,14 +8,39 @@ variable "GOOGLE_CREDENTIALS" {
   description = "GCP Credentials path or json format text"
 }
 
+variable "mainly_region" {
+  type        = string
+  default     = "us-west1"
+  description = "GCP region to be used mainly"
+}
+
+variable "mainly_zone" {
+  type        = string
+  default     = "us-west1-c"
+  description = "GCP zone to be used mainly"
+}
+
 variable "terraform_cloud_organization" {
   type        = string
   default     = "x-kubernetes"
-  description = "Terraform Cloud organization name"
+  description = "Organization name in Terraform Cloud"
 }
 
 variable "terraform_cloud_workspace" {
   type        = string
   default     = "x-kubernetes"
-  description = "Terraform Cloud workspace name"
+  description = "Workspace name in Terraform Cloud"
+}
+
+locals {
+  master_nodes = {
+    master1 = false
+    master2 = true
+    master3 = true
+  }
+  worker_nodes = {
+    worker1 = false
+    worker2 = true
+    worker3 = true
+  }
 }
